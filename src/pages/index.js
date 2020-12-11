4b245fe96a534d7cac60e758fb5afd48727927d7
Kevin Ng <ngkev5@gmail.com>
2020-12-10 21:54:13 -0800

    removing header text for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,7 @@ import '../styles/reset.scss'
 const IndexPage = () => {
   return (
     <PageLayout pageTitle="Home">
-      <h2>Hello.</h2>
-      <h3>I'm Kevin, a full-stack web applications developer in Vancouver, BC.</h3>
+      Nothing to see yet.
     </PageLayout>
   )
 }
